d536b46b55bb17fd2b56e1789de96096c303bb49
Ferrel John Fernando <ferreluchiha@gmail.com>
2022-03-22 01:07:47 +0700

    added endpoint for load testing

--- nest-fastify/src/app.service.ts
+++ nest-fastify/src/app.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { readFileSync } from 'fs';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async getUsers() {
+    const min = 0,
+      max = 910;
+    const users = JSON.parse(readFileSync('users.json', 'utf-8'));
+    const index = Math.floor(Math.random() * (max - min + 1) + min);
+    return users[index];
   }
 }
